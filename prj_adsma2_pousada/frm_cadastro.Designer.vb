659da7ed07af4dcc5aea2a2d91dd89de145eef23
Everton-Victor <you@example.com>
2022-05-30 10:19:17 -0300

    Form Cadastro Funcionarios
    Parte 1 - Menu Design

---
+++ prj_adsma2_pousada/frm_cadastro.Designer.vb
@@ -0,0 +1,170 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frm_cadastro
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frm_cadastro))
+        Dim CheckoutToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.tab_cadastro = New System.Windows.Forms.TabPage()
+        Me.tab_lista = New System.Windows.Forms.TabPage()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.ToolStripDropDownButton1 = New System.Windows.Forms.ToolStripDropDownButton()
+        Me.ReservaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CheckinToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PacoteDeServiçosToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.QuartosToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EncerrarSessToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        CheckoutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TabControl1.SuspendLayout()
+        Me.ToolStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.tab_cadastro)
+        Me.TabControl1.Controls.Add(Me.tab_lista)
+        Me.TabControl1.Location = New System.Drawing.Point(19, 48)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(791, 443)
+        Me.TabControl1.TabIndex = 0
+        '
+        'tab_cadastro
+        '
+        Me.tab_cadastro.Font = New System.Drawing.Font("Cambria", 10.0!)
+        Me.tab_cadastro.Location = New System.Drawing.Point(4, 22)
+        Me.tab_cadastro.Name = "tab_cadastro"
+        Me.tab_cadastro.Padding = New System.Windows.Forms.Padding(3)
+        Me.tab_cadastro.Size = New System.Drawing.Size(783, 417)
+        Me.tab_cadastro.TabIndex = 0
+        Me.tab_cadastro.Text = "Cadastro"
+        Me.tab_cadastro.UseVisualStyleBackColor = True
+        '
+        'tab_lista
+        '
+        Me.tab_lista.Font = New System.Drawing.Font("Cambria", 10.0!)
+        Me.tab_lista.Location = New System.Drawing.Point(4, 22)
+        Me.tab_lista.Name = "tab_lista"
+        Me.tab_lista.Padding = New System.Windows.Forms.Padding(3)
+        Me.tab_lista.Size = New System.Drawing.Size(783, 417)
+        Me.tab_lista.TabIndex = 1
+        Me.tab_lista.Text = "Lista"
+        Me.tab_lista.UseVisualStyleBackColor = True
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.ImageScalingSize = New System.Drawing.Size(35, 35)
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripDropDownButton1})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(828, 42)
+        Me.ToolStrip1.TabIndex = 1
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'ToolStripDropDownButton1
+        '
+        Me.ToolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripDropDownButton1.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ReservaToolStripMenuItem, Me.PacoteDeServiçosToolStripMenuItem, Me.QuartosToolStripMenuItem, Me.EncerrarSessToolStripMenuItem})
+        Me.ToolStripDropDownButton1.Image = CType(resources.GetObject("ToolStripDropDownButton1.Image"), System.Drawing.Image)
+        Me.ToolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripDropDownButton1.Name = "ToolStripDropDownButton1"
+        Me.ToolStripDropDownButton1.Size = New System.Drawing.Size(48, 39)
+        Me.ToolStripDropDownButton1.Text = "ToolStripDropDownButton1"
+        '
+        'ReservaToolStripMenuItem
+        '
+        Me.ReservaToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CheckinToolStripMenuItem, CheckoutToolStripMenuItem})
+        Me.ReservaToolStripMenuItem.Image = CType(resources.GetObject("ReservaToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.ReservaToolStripMenuItem.Name = "ReservaToolStripMenuItem"
+        Me.ReservaToolStripMenuItem.Size = New System.Drawing.Size(199, 42)
+        Me.ReservaToolStripMenuItem.Text = "Reserva"
+        '
+        'CheckinToolStripMenuItem
+        '
+        Me.CheckinToolStripMenuItem.Image = CType(resources.GetObject("CheckinToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.CheckinToolStripMenuItem.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.CheckinToolStripMenuItem.Name = "CheckinToolStripMenuItem"
+        Me.CheckinToolStripMenuItem.Size = New System.Drawing.Size(199, 42)
+        Me.CheckinToolStripMenuItem.Text = "Check-in"
+        '
+        'CheckoutToolStripMenuItem
+        '
+        CheckoutToolStripMenuItem.Image = CType(resources.GetObject("CheckoutToolStripMenuItem.Image"), System.Drawing.Image)
+        CheckoutToolStripMenuItem.Name = "CheckoutToolStripMenuItem"
+        CheckoutToolStripMenuItem.Size = New System.Drawing.Size(199, 42)
+        CheckoutToolStripMenuItem.Text = "Check-out"
+        '
+        'PacoteDeServiçosToolStripMenuItem
+        '
+        Me.PacoteDeServiçosToolStripMenuItem.Image = CType(resources.GetObject("PacoteDeServiçosToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.PacoteDeServiçosToolStripMenuItem.Name = "PacoteDeServiçosToolStripMenuItem"
+        Me.PacoteDeServiçosToolStripMenuItem.Size = New System.Drawing.Size(199, 42)
+        Me.PacoteDeServiçosToolStripMenuItem.Text = "Pacote de serviços"
+        '
+        'QuartosToolStripMenuItem
+        '
+        Me.QuartosToolStripMenuItem.Image = CType(resources.GetObject("QuartosToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.QuartosToolStripMenuItem.Name = "QuartosToolStripMenuItem"
+        Me.QuartosToolStripMenuItem.Size = New System.Drawing.Size(199, 42)
+        Me.QuartosToolStripMenuItem.Text = "Quartos"
+        '
+        'EncerrarSessToolStripMenuItem
+        '
+        Me.EncerrarSessToolStripMenuItem.Image = CType(resources.GetObject("EncerrarSessToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.EncerrarSessToolStripMenuItem.Name = "EncerrarSessToolStripMenuItem"
+        Me.EncerrarSessToolStripMenuItem.Size = New System.Drawing.Size(199, 42)
+        Me.EncerrarSessToolStripMenuItem.Text = "Encerrar sessão"
+        '
+        'frm_cadastro
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(198, Byte), Integer), CType(CType(187, Byte), Integer), CType(CType(187, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(828, 503)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Controls.Add(Me.TabControl1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "frm_cadastro"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Funcionários"
+        Me.TabControl1.ResumeLayout(False)
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents tab_cadastro As TabPage
+    Friend WithEvents tab_lista As TabPage
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents ToolStripDropDownButton1 As ToolStripDropDownButton
+    Friend WithEvents ReservaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CheckinToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PacoteDeServiçosToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents QuartosToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EncerrarSessToolStripMenuItem As ToolStripMenuItem
+
+End Class
