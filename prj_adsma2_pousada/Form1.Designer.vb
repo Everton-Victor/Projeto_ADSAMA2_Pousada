f701ae6735908db8270d39c54bb562de7ecbb127
Everton-Victor <you@example.com>
2022-05-30 09:32:58 -0300

    Tela Login

--- prj_adsma2_pousada/Form1.Designer.vb
+++ prj_adsma2_pousada/Form1.Designer.vb
@@ -22,10 +22,86 @@ Partial Class Form1
     'Não o modifique usando o editor de códigos.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txt_senha = New System.Windows.Forms.TextBox()
+        Me.btn_entrar = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txt_email = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Cambria", 22.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 103)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(102, 36)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Senha:"
+        '
+        'txt_senha
+        '
+        Me.txt_senha.Font = New System.Drawing.Font("Cambria", 18.0!)
+        Me.txt_senha.Location = New System.Drawing.Point(18, 141)
+        Me.txt_senha.Name = "txt_senha"
+        Me.txt_senha.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txt_senha.Size = New System.Drawing.Size(480, 36)
+        Me.txt_senha.TabIndex = 10
+        '
+        'btn_entrar
+        '
+        Me.btn_entrar.BackColor = System.Drawing.Color.FromArgb(CType(CType(96, Byte), Integer), CType(CType(69, Byte), Integer), CType(CType(69, Byte), Integer))
+        Me.btn_entrar.Font = New System.Drawing.Font("Cambria", 20.0!, System.Drawing.FontStyle.Bold)
+        Me.btn_entrar.ForeColor = System.Drawing.Color.White
+        Me.btn_entrar.Location = New System.Drawing.Point(360, 229)
+        Me.btn_entrar.Name = "btn_entrar"
+        Me.btn_entrar.Size = New System.Drawing.Size(138, 50)
+        Me.btn_entrar.TabIndex = 9
+        Me.btn_entrar.Text = "Entrar"
+        Me.btn_entrar.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Cambria", 22.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(106, 36)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "E-mail:"
+        '
+        'txt_email
+        '
+        Me.txt_email.Font = New System.Drawing.Font("Cambria", 18.0!)
+        Me.txt_email.Location = New System.Drawing.Point(18, 47)
+        Me.txt_email.Name = "txt_email"
+        Me.txt_email.Size = New System.Drawing.Size(480, 36)
+        Me.txt_email.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(198, Byte), Integer), CType(CType(187, Byte), Integer), CType(CType(187, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(530, 311)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txt_senha)
+        Me.Controls.Add(Me.btn_entrar)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txt_email)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Login"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txt_senha As TextBox
+    Friend WithEvents btn_entrar As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txt_email As TextBox
 End Class
