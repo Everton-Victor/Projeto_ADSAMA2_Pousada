3fcf2e79050ddff873969e379b87e83a3c962cf1
Everton-Victor <evertonvictor2019br@gmail.com.br>
2022-06-21 19:47:36 -0300

    ok

--- prj_adsma2_pousada/My Project/Application.Designer.vb
+++ prj_adsma2_pousada/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.prj_adsma2_pousada.frm_login
+            Me.MainForm = Global.prj_adsma2_pousada.frm_quartos
         End Sub
     End Class
 End Namespace
